7476e8d2c2fbcc986ad82b75d2f2858142eea99f
Daniel <daniel_dp96@hotmail.com>
2021-01-14 13:25:07 -0600

    Aplicacion terminada

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+import { LoginScreen } from "../components/login/LoginScreen";
+import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
+
+export const AppRouter = () => {
+
+  const { user: { logged } } = useContext(AuthContext); 
+
+  return (
+    <Router>
+      <div>
+        {/* <Navbar /> */}
+
+        <Switch>
+          <PublicRoute isAutenticated={ logged } exact path="/login" component={LoginScreen} />
+          <PrivateRoute isAutenticated={ logged } path="/" component={ DashboardRoutes } />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
